01175a3a48bf6e9f6c7b706d11ab9362240cdc8f
Kyzym Julia <102462052+KizimJule@users.noreply.github.com>
2022-11-08 18:25:09 +0100

    Initial commit

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import {
+  ContactListUl,
+  ContactListLi,
+  ButtonDelete,
+} from './ContactList.styled';
+import { FcPhoneAndroid } from 'react-icons/fc';
+// import AddIcCallIcon from '@material-ui/icons/AddIcCall';
+
+export const ContactList = ({ contacts, deleteContact }) => (
+  <ContactListUl>
+    {contacts.map((contact, id) => (
+      <ContactListLi key={id}>
+        <FcPhoneAndroid />
+        {contact.name}: {contact.number}
+        <ButtonDelete type="button" onClick={() => deleteContact(contact.id)}>
+          Delete
+        </ButtonDelete>
+      </ContactListLi>
+    ))}
+  </ContactListUl>
+);
+ContactList.propTypes = {
+  deleteContact: propTypes.func.isRequired,
+  contacts: propTypes.arrayOf(
+    propTypes.exact({
+      id: propTypes.string.isRequired,
+      name: propTypes.string.isRequired,
+      number: propTypes.string.isRequired,
+    })
+  ),
+};
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { SectionContainer, Title } from './Section.style';
+
+export const Section = ({ title, children }) => (
+  <SectionContainer>
+    <Title>{title}</Title>
+    {children}
+  </SectionContainer>
+);
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+};
